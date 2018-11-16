1e8e59e5e86b1d5432045edf2531a4f612c3fab9
konstantin.koryaka <konstantin.koryaka@state.ca.gov>
2018-11-16 11:33:10 -0800

    Added custom widget position to the footer

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@ wp_footer();
 if ( ! is_404()) :
 ?>
 
-<script> 
+<script>
 window.et_pb_smooth_scroll=function($target,$top_section,speed,easing){
                 var $window_width=$(window).width();
                 $("header").hasClass("fixed")&&$window_width>768?$menu_offset=$("#header").outerHeight()-1:$menu_offset=-1,
@@ -29,27 +29,30 @@ window.et_pb_smooth_scroll=function($target,$top_section,speed,easing){
 
 <?php if (is_tag() || is_archive() || is_category() || is_author()) : ?>
   <script>
-    
- jQuery(document).ready(function() {   
+
+ jQuery(document).ready(function() {
    		var articles = document.getElementsByTagName('main')[0].getElementsByTagName('article');
    		var makeSpace = false;
-   
+
        for(var i = 0, len = articles.length; i < len; i++){
          if( articles[i].classList.contains('has-post-thumbnail')){
            makeSpace = true;
 	       }
        }
-   
+
    if(makeSpace){
      for(var i = 0, len = articles.length; i < len; i++){
        if( ! articles[i].classList.contains('has-post-thumbnail'))
          articles[i].getElementsByTagName('a')[0].setAttribute("style", "width:200px;height:150px;padding-right:20px;padding-bottom:15px;float:left;");
-       
+
        }
    }
-   
+
  });
-    	
+
   </script>
 
 <?php endif; ?>
+
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Widget custom") ) : ?>
+<?php endif;?>
