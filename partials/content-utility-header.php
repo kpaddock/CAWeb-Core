21e8e4355e922de0add350540d5be9170d2cf0ec
konstantin.koryaka <konstantin.koryaka@state.ca.gov>
2018-11-16 07:56:28 -0800

    Updated mobile nav button

--- partials/content-utility-header.php
+++ partials/content-utility-header.php
@@ -1,10 +1,11 @@
-<?php      $gtranslator = get_option('ca_google_trans_enabled'); ?>    
+<?php      $gtranslator = get_option('ca_google_trans_enabled'); ?>
 <div class="utility-header ">
     <div class="container">
       <div class="group">
-            <div class="quarter social-links">
-              <a href="http://www.ca.gov/"  title="CA.gov"style="float: left;"><img style="height: 31px;" src="<?php print get_stylesheet_directory_uri();?>/images/system/logo.svg" alt="Image of the CA.gov Logo"/></a>
+            <div class="quarter">
                 <ul class="utility-links social-media-links">
+<li><div class="header-cagov-logo"><a href="http://www.ca.gov/"  title="CA.gov"style="float: left;"><img src="<?php print get_stylesheet_directory_uri();?>/images/system/logo.svg" alt="Image of the CA.gov Logo"/></a></div></li>
+
 					<?php
               $gtranslator = get_option('ca_google_trans_enabled') || 'standard' == get_option('ca_google_trans_enabled') || 'custom' == get_option('ca_google_trans_enabled') ? true : false;
 
@@ -31,7 +32,7 @@
             </div>
             <div class="<?php print $gtranslator ? 'half' : 'three-quarters' ?> settings-links hidden-print">
                 <ul class="utility-links ">
-                  
+
 					<?php
 					  for ($i = 1; $i < 4; $i++) {
 					      $url = get_option(sprintf('ca_utility_link_%1$s', $i));
@@ -46,8 +47,8 @@
 					?>
                   <?php if ("" !== get_option('ca_contact_us_link')): ?>
                     <li class="utility-contact-us"><a href="<?php print get_option('ca_contact_us_link'); ?>">Contact Us</a></li>
-                  <?php endif; ?> 
-                  
+                  <?php endif; ?>
+
                   <!--li class="utility-settings"><a role="button" data-toggle="collapse" href="#siteSettings"  aria-expanded="false" aria-controls="siteSettings" >Settings</a></li-->
                   <li class="utility-settings">
                   <button class="btn btn-xs btn-primary collapsed" data-toggle="collapse" href="#siteSettings" aria-expanded="false" aria-controls="siteSettings"><span class="ca-gov-icon-gear" aria-hidden="true"></span> Settings</button>
@@ -58,12 +59,12 @@
                     <?php endif; ?>
 										<?php if ('custom' == get_option('ca_google_trans_enabled') && "" !== get_option('ca_google_trans_page', '')): ?>
 				              <a id="caweb-gtrans-custom" target="_blank" href="<?php print esc_url(get_option('ca_google_trans_page')) ?>"><?php print "" !== get_option('ca_google_trans_icon') ? caweb_get_icon_span(get_option('ca_google_trans_icon')) : '' ?>Translate</a>
-				            <?php endif; ?> 
-              </ul>              
-            </div> 
+				            <?php endif; ?>
+              </ul>
+            </div>
             <?php if (true === get_option('ca_google_trans_enabled') || 'standard' == get_option('ca_google_trans_enabled')): ?>
               <div class="quarter standard-translate" id="google_translate_element"></div>
-            <?php endif; ?>						  
-        </div>          
+            <?php endif; ?>
+        </div>
     </div>
 </div>
